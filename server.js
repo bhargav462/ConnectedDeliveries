a9bae0a147cb511df5c58de45f173699736d2614
Nakkina Bhargav <43701429+bhargav462@users.noreply.github.com>
2020-03-26 23:50:40 +0530

    index header

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(__dirname + '/playground'))
+app.use(express.static(__dirname + '/public'))
 
 app.get('/',(req,res) => {
-    res.sendFile('header.html',{root:__dirname + '/playground'})
+    res.sendFile('index.html',{root:__dirname + '/public'})
 })
 
 var server = app.listen(3000,(res) => {
